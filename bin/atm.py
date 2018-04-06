6a5ba68980c3212d475969e1e6624c53a4ed5683
root <root@moble-work>
2018-04-06 11:06:05 +0800

    add

---
+++ bin/atm.py
@@ -0,0 +1,8 @@
+import os,sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+def a():
+    pass
---
+++ core/main.py
@@ -0,0 +1,51 @@
+import sys,os
+
+user_data = {
+    "account_id":None,
+    "is_authenticated":False,
+    "account_data":None
+}
+
+
+def select():
+    pass
+def add():
+    pass
+def remove():
+    pass
+def transfer():
+    pass
+def withdraw():
+    pass
+def repay():
+    pass
+def frozen():
+    pass
+def printbill():
+    pass
+
+def run():
+    print(menu)
+
+menu = u'''
+------- Alex huang Bank ---------
+\033[32;1m1.  账户信息
+2.  添加账户
+3.  注销账户
+4.  转账
+5.  提现
+6.  冻结解冻
+7.  返款
+8.  打印账单
+\033[0m'''
+menu_dic = {
+    "1":select,
+    "2":add,
+    "3":remove,
+    "4":transfer,
+    "5":withdraw,
+    "6":frozen,
+    "7":repay,
+    "8":printbill
+}
+run()
