617371f1c918b3c59561f9aeb05f02d4d8ff9975
alex huang <root@localhost>
2018-04-11 18:58:30 +0800

    add

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # author aliex-hrg
-import time
+import xml.etree.ElementTree as ET
 
-print(time.time())
+new_xml = ET.Element("namelist")
 
-print(time.mktime(time.strptime("2014-1-1",'%Y-%m-%d')))
-while True:
-    a = input().strip()
-    print(a.isdecimal())
-    print(float(a))
+name1 = ET.SubElement(new_xml, "info", attrib={"enrolled": "yes"})
+name = ET.SubElement(name1, "name")
+name.text = '22'
+age = ET.SubElement(name1, "age", attrib={"checked": "no"})
+sex = ET.SubElement(name1, "sex")
+sex.text = '33'
+
+name2 = ET.SubElement(new_xml, "info", attrib={"enrolled": "no"})
+age = ET.SubElement(name2, "age")
+age.text = '19'
+
+
+et = ET.ElementTree(new_xml)  # 生成文档对象
+et.write("test.xml", encoding="utf-8", xml_declaration=True)
+
+ET.dump(new_xml)  # 打印生成的格式
