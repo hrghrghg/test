ea96cc4be05d208c517aa63708071f0cd94a0c32
root <root@moble-work>
2018-04-18 23:12:02 +0800

    add

--- test.py
+++ test.py
@@ -4,6 +4,32 @@
 import re,os
 import sys
 
-k = [i for i in '1, 2, 3,1,2,5,6aaas' if i]
+class School(object):
+    number = 0
+    def __init__(self,name,age,sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.enroll()
+    def enroll(self):
+        print('%s join school' %self.name)
+        School.number += 1
+    def info(self):
+        print('--------info:',self.name)
+        for k,v in self.__dict__.items():
+            print(k,v)
+        print('--------end')
+class Teacher(School):
+    def __init__(self,name,age,sex,salary,courge):
+        School.__init__(self,name,age,sex)
+        self.salary = salary
+        self.courge = courge
 
-print(k,k[-1])
+    def get_salary(self,amount):
+        print('Teacher %s withdraw salary:%s' %(self.name,amount))
+
+t1 = Teacher("lixiang",18,"F",3000,"python")
+t2 = Teacher("wuji",12,"M",4000,"linux")
+t3 = Teacher("zhansan",38,"F",30000,"python")
+t1.info()
+print(School.number)
