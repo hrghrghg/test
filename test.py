5cdccdae70a055f042cd4f08ee5c1b7a99e37a86
alex huang <root@localhost>
2018-04-19 11:29:25 +0800

    add

--- test.py
+++ test.py
@@ -19,17 +19,38 @@ class School(object):
         for k,v in self.__dict__.items():
             print(k,v)
         print('--------end')
+    def __del__(self):
+        print('School del ....')
 class Teacher(School):
-    def __init__(self,name,age,sex,salary,courge):
+    def __init__(self,name,age,sex,salary,course):
         School.__init__(self,name,age,sex)
         self.salary = salary
-        self.courge = courge
-
+        self.course = course
+        self.amount = 3000
     def get_salary(self,amount):
         print('Teacher %s withdraw salary:%s' %(self.name,amount))
-
+        self.amount -= amount
+    def __del__(self):
+        print('Teacher del ....')
+        School.number -= 1
+class Student(School):
+    def __init__(self,name,age,sex,tuition,course):
+        School.__init__(self,name,age,sex)
+        self.tuition = tuition
+        self.course = course
+        self.amount = tuition
+    def sign_up(self):
+        print("%s student sign up %s,tuition is:%s" %(self.name,self.course,self.tuition))
+        self.amount -= self.tuition
 t1 = Teacher("lixiang",18,"F",3000,"python")
 t2 = Teacher("wuji",12,"M",4000,"linux")
-t3 = Teacher("zhansan",38,"F",30000,"python")
-t1.info()
+# t3 = Teacher("zhansan",38,"F",30000,"python")
+s1 = Student('lihao',10,"F",1000,"python")
+# s2 = Student('daguo',12,"M",1200,"java")
 print(School.number)
+del t2
+print(School.number)
+
+# s2.sign_up()
+# print(s2.amount)
+# print(s1.amount)
