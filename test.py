48143eaf57fb577b3fde6d2bc08f7d145e6f1dd6
alex huang <root@localhost>
2018-04-23 18:17:36 +0800

    add

--- test.py
+++ test.py
@@ -4,60 +4,17 @@
 import re,os
 import sys
 
-class School(object):
-    number = 0
-    def __init__(self,name,age,sex):
-        self.name = name
-        self.age = age
-        self.sex = sex
-        self.enroll()
-    def enroll(self):
-        print('%s join school' %self.name)
-        School.number += 1
-    def info(self):
-        print('--------info:',self.name)
-        for k,v in self.__dict__.items():
-            print(k,v)
-        print('--------end')
-    def __del__(self):
-        print('School del ....')
-class Teacher(School):
-    def __init__(self,name,age,sex,salary,course):
-        #School.__init__(self,name,age,sex)
-        super(Teacher,self).__init__(name,age,sex)
-        self.salary = salary
-        self.course = course
-        self.amount = 3000
-    def get_salary(self,amount):
-        print('Teacher %s withdraw salary:%s' %(self.name,amount))
-        self.amount -= amount
-    def __del__(self):
-        print('Teacher del ....')
-        School.number -= 1
-    def talk(self):
-        print('meoa meoa')
-class Student(School):
-    def __init__(self,name,age,sex,tuition,course):
-        School.__init__(self,name,age,sex)
-        self.tuition = tuition
-        self.course = course
-        self.amount = tuition
-    def sign_up(self):
-        print("%s student sign up %s,tuition is:%s" %(self.name,self.course,self.tuition))
-        self.amount -= self.tuition
-    def talk(self):
-        print('woof woof')
+class a(object):
+    def __init__(self,name):
+        self.name1 = name
+    name = 'afd'
+    def aa(self,name):
+        global name2 = 'a2a'
 
-def talk2(obj):
-    return obj.talk()
-t1 = Teacher("lixiang",18,"F",3000,"python")
-t2 = Teacher("wuji",12,"M",4000,"linux")
-# t3 = Teacher("zhansan",38,"F",30000,"python")
-s1 = Student('lihao',10,"F",1000,"python")
-# s2 = Student('daguo',12,"M",1200,"java")
-talk2(t1)
-talk2(s1)
+    @classmethod
+    def b(self):
+        print("kk",self.name2)
 
-# s2.sign_up()
-# print(s2.amount)
-# print(s1.amount)
+
+a=a('aa')
+a.b()
--- 作业/选课系统/src/modules.py
+++ 作业/选课系统/src/modules.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # author aliex-hrg
-import os,sys
+import os,sys,pickle
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 from conf import settings
 import identified
 
 class Basemod(object):
-    pass
+
+    def save(self):
+        filepath = os.path.join(self.db_path,self.nid.uuid)
+        with open(filepath,'wb') as f:
+            pickle.dump(self,f)
+    @classmethod
+    def get_all_obj_list(cls):
+        ret=[]
+        for filename in os.listdir(cls.db_path):
+            file_path = os.path.join(cls.db_path,filename)
+            obj = pickle.load(open(file_path,'rb'))
+            ret.append(obj)
+        return ret
 
 class Admin(Basemod):
     db_path = settings.ADMIN_PATH
@@ -17,27 +29,64 @@ class Admin(Basemod):
         self.username = username
         self.password = passwd
 
+    def login(self):
+        pass
+
 class School(Basemod):
     db_path = settings.SCHOOL_PATH
-    pass
+    def __init__(self,name,addr):
+        self.nid = identified.School_Nid(self.db_path)
+        self.name = name
+        self.addr = addr
+        self.__income = 0
+        self.save()
+    def __str__(self):
+        return self.name
+
 class Classes(Basemod):
     db_path = settings.CLASSES_PATH
     pass
 class Course(Basemod):
     db_path = settings.COURSE_PATH
-    pass
+    def __init__(self,name,price,period,school_nid):
+        self.nid = identified.Course_Nid(self.db_path)
+        self.name = name
+        self.price = price
+        self.period =period
+        self.school_nid = school_nid.uuid
+        self.save()
+
+class Course_to_Teacher(Basemod):
+    db_path = settings.COURSE_TO_PATH
+    def __init__(self,course_nid,teacher_nid):
+        self.nid = identified.Course_to_teacher_Nid()
+        self.course_nid =course_nid
+        self.teacher_nid = teacher_nid
+    def get_course_to_teacher_list(self):
+        ret = self.get_all_obj_list()
+        res = [(obj.course_nid.get_obj_by_uuid(),obj.teacher_nid.get_obj_by_uuid())\
+               for obj in ret]
+        return res
 class Teacher(Basemod):
     db_path = settings.TEACHER_PATH
-    pass
+    def __init__(self,name,level):
+        self.nid = identified.Teacher_Nid(self.db_path)
+        self.name = name
+        self.level = level
+        self.__account = 0
+
 class Student(Basemod):
     db_path = settings.STUDENT_PATH
     pass
 class Score(Basemod):
 #    db_path = settings.SCORE_PATH
     pass
-class Course_to_Student(Basemod):
-    db_path = settings.COURSE_TO_PATH
-    pass
 
 
 
+if __name__ == '__main__':
+    s1 = School("shanghai","abc")
+#    s1.save(os.path.join(settings.SCHOOL_PATH,s1.nid.uuid))
+    c1 = Course("python","5800","4m",s1.nid)
+    for i in c1.get_all_obj_list():
+        print(i.name)
