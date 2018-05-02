89653a6b94f7f11940f53c04b211398644300845
root <root@moble-work>
2018-05-02 22:46:18 +0800

    add

--- test3.py
+++ test3.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # author aliex-hrg
+import sys,os,time
 c = "k"
 m = {}
 m["s"] = 1
 m["b"] = 2
 m[c] = 3
+width = 50
+percent = 1
+show_str=('[%%-%ds]' %width) %(int(width*percent) * '#')
+print('\r%s %d%%' %(show_str,int(100*percent)),file=sys.stdout,flush=True,end='')
 
-import sys, time
-
-for i in range(100):
-    sys.stdout.write('.')
-    sys.stdout.flush()
-    #time.sleep(1)
-
-print()
-print('dum')
+show_str = "[%-100s]" %(int(percent)*'#')
+print(show_str)
--- 作业/FTP文件服务器/client/core/interactive.py
+++ 作业/FTP文件服务器/client/core/interactive.py
@@ -33,6 +33,7 @@ def interactive(conn,username):
             if filesize != 0:
                 m = hashlib.md5()
                 filenum = 0
+                progress = 0
                 print(filesize)
                 conn.send(b"ack")  #消除粘包
                 if info_dict.get('par1'):
@@ -41,10 +42,17 @@ def interactive(conn,username):
                             data = conn.recv(8096)
                             f.write(data)
                             filenum += len(data)
-                            percent = '{:.0f}'.format(filenum/filesize*100)
-                            print(percent)
-                            print("{:.2f}%".format(filenum/filesize*100))
-                            print('sum:',filenum,'curr:',len(data))
+                            modulus = (len(data)+progress)//(filesize/100)  #把一次读取到的大小按文件总大小除100取模，并打印模个点
+                            if modulus >= 1.0:progress = 0    #如果模为0，累计到下次，直到模为1，并清除累计值
+                            progress = (len(data)+progress)%(filesize/100)    #余数 累计值
+                            for i in range(int(modulus)):    #for打印一次模的点数
+                                sys.stdout.write('.')
+                                sys.stdout.flush()
+                            percent = int(100*filenum/filesize)
+                            show_str = "[%-100s] %s%%" %(percent*'#',percent)
+                            print('\r%s' %show_str,file=sys.stdout,flush=True,end='')
+                            # print("{:.2f}%".format(filenum/filesize*100))
+                            # print('sum:',filenum,'curr:',len(data))
 
         re_data = conn.recv(8096).decode()
         print(re_data)
